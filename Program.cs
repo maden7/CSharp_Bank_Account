df9f43e0bcc0d8f49517a0eebda18938a2f98fd0
maden7 <108528810+maden7@users.noreply.github.com>
2023-02-07 16:48:12 +0000

    Added something

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace classes
         {
             var account = new BankAccount("Patrick Lehmann", 1000);
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+            account.MakeWithdrawal(100, DateTime.Now, "Rent payment");
 
             account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
             Console.WriteLine(account.Balance);
